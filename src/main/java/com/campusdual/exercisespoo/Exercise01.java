cc6f7a3e260ba479c9e62875e3433d1d271bd267
AlbertoPomboParga <148323956+AlbertoPomboParga@users.noreply.github.com>
2023-10-19 09:22:27 +0200

    add exercise 1

---
+++ src/main/java/com/campusdual/exercisespoo/Exercise01.java
@@ -0,0 +1,26 @@
+package com.campusdual.exercisespoo;
+
+public class Exercise01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        String cnv = "200";
+        int cnvInt = Integer.parseInt(cnv);
+        cnv=cnv+1;
+        cnvInt = cnvInt +1;
+        String cnvStr = String.valueOf(cnvInt);
+        char cnvChar =cnvStr.charAt(2);
+
+
+        int i=1;
+        String iStr = String.valueOf(i);
+        Double iDouble = Double.valueOf(iStr);
+        String iDStr = String.valueOf(iDouble)
+        System.out.println(Boolean.valueOf("true"));
+        System.out.println(String.valueOf(false));
+
+
+        //1= "1" = 1.0 = "true"
+        //0= "0" = 0.0 = "false"
+        //2= "2" = 2.00 = "true"
+}
